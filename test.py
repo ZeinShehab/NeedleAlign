5ab2529d2634ff3ee6ba8b55cdaa060cecd919e2
ZeinShehab <zeinshehab@outlook.com>
2024-12-03 23:20:00 +0200

    removed unused files

--- test.py
+++
@@ -1,9 +0,0 @@
-from Bio import pairwise2
-from Bio.pairwise2 import format_alignment
-
-
-
-
-alignments = pairwise2.align.globalms(v, w, match_score, mismatch_penalty, gap_open_penalty, gap_extend_penalty)
-for alignment in alignments:
-    print(format_alignment(*alignment))
